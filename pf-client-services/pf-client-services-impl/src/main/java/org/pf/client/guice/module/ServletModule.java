914cd2e63f842f34138ab24206acec858bc794de
narendranjs <narendran.js@gmail.com>
2016-06-20 11:03:43 -0500

    added modules

---
+++ pf-client-services/pf-client-services-impl/src/main/java/org/pf/client/guice/module/ServletModule.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package org.pf.client.guice.module;
+
+import java.util.Map;
+
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.MessageBodyWriter;
+
+import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
+import org.pf.client.service.jersey.PeopleFinderResourceJersey;
+import org.pf.client.services.api.services.jersey.PeopleFinderResource;
+
+import com.google.common.collect.Maps;
+import com.google.inject.Singleton;
+import com.sun.jersey.api.json.JSONConfiguration;
+import com.sun.jersey.guice.JerseyServletModule;
+import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
+
+/**
+ * @author U25276
+ *
+ */
+public class ServletModule extends JerseyServletModule {
+	
+	@Override
+	protected void configureServlets() {
+		bind(PeopleFinderResource.class).to(PeopleFinderResourceJersey.class).in(Singleton.class);
+		
+		bind(MessageBodyReader.class).to(JacksonJsonProvider.class);
+		bind(MessageBodyWriter.class).to(JacksonJsonProvider.class);
+		
+		Map<String, String> params=Maps.newHashMap();
+		params.put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE.toString());
+		serve("/rest/*").with(GuiceContainer.class, params);
+	}
+}
